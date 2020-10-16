11652aeedeb0d1a8e77289e4f85d727e83818517
ccurnuck <calvin.curnuck@gmail.com>
2020-10-16 16:18:42 -0400

    Added Comments

--- features/websocket_features.js
+++ features/websocket_features.js
@@ -9,9 +9,10 @@ module.exports = function(controller) {
         console.log('Loading sample web features...');
 
         controller.hears(new RegExp('quick'), 'message', async (bot, message) => {
-
+            // when quick is input detected, will offer these quick options
             await bot.reply(message,{
                 text: 'Here are some quick replies',
+                // use main or key categories Ex. resume / skills / projects / experience / interests etc
                 quick_replies: [
                     {
                         title: 'Foo',
