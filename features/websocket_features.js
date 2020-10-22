e31b5b92ed2f52b4724a5d01a1eebc6d87aa0dc6
ccurnuck <calvin.curnuck@gmail.com>
2020-10-22 13:21:18 -0400

    Fits custom resume skills

--- features/websocket_features.js
+++ features/websocket_features.js
@@ -7,34 +7,15 @@ module.exports = function(controller) {
     if (controller.adapter.name === 'Web Adapter') {
 
         console.log('Loading sample web features...');
-
         // use main or key categories Ex. resume / skills / projects / experience / interests etc
         controller.hears(new RegExp('skills'), 'message', async (bot, message) => {
             // when quick is input detected, will offer these quick options
             await bot.reply(message,{
                 text: 'Here are some of my skills.',
-                quick_replies: [
-                    {
-                        title: 'JavaScript',
-                        payload: 'javascript',
-                    },
-                    {
-                        title: 'Rails',
-                        payload: 'rails',
-                    },
-                    {
-                        title: 'Ruby',
-                        payload: 'ruby',
-                    },
-                    {
-                        title: 'MongoDB',
-                        payload: 'mongodb',
-                    },
-                    {
-                        title: 'PostgreSQL',
-                        payload: 'postgres',
-                    }
-                ]
+                quick_replies: resume.skills.keywords.map(skill => ({
+                    title: skill,
+                    payload: skill.toLowerCase(),
+                }))
             });
         });
 
